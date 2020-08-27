fdb77b62938e317b14aee63394d99bd65f1527bb
ishrataraamin09 <67515063+ishrataraamin09@users.noreply.github.com>
2020-08-27 17:16:53 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect,} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:'coral'}}>Welcome to Online Academy.</h1>
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/fakeData/courses.js
@@ -0,0 +1,94 @@
+var courses = [
+    {
+        name: 'Physics' ,
+        id: 1,
+        category: 'Science',
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'Chemistry' ,
+        id: 2,
+        category: 'Science' ,
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'Biology' ,
+        id: 3,
+        category: 'Science',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'Economics' ,
+        id: 4,
+        category: 'Commerce',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'Additional Mathematics' ,
+        id: 5,
+        category: 'General',
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'English Language' ,
+        id: 6,
+        category: 'General',
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'General Mathematics' ,
+        id: 7,
+        category: 'General',
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'Accounting' ,
+        id: 8,
+        category: 'Commerce',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'Human Biology' ,
+        id: 9,
+        category: 'Pure-Science',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'ICT' ,
+        id: 10,
+        category: 'Computer-Science',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'Bengali Language' ,
+        id: 11,
+        category: 'Optional',
+        totalCourseFee = 200 
+    } ,
+    {
+        name: 'Social Science' ,
+        id: 12,
+        category: 'Optional' ,
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'Computer Architecture & Organisation' ,
+        id: 13,
+        category: 'Computer-Science' ,
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'History' ,
+        id: 14,
+        category: 'Optional' ,
+        totalCourseFee = 200
+    } ,
+    {
+        name: 'English Literature' ,
+        id: 15,
+        category: 'General' ,
+        totalCourseFee = 200
+    }
+]
+
+export default courses ;
